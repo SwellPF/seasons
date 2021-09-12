af4e8de64c9f2e646ed4b60a6013e381857fa7b6
Paul Fell <swellpf@gmail.com>
2021-09-12 15:16:24 -0400

    refactored for componentDidMount to get geolocation

--- src/index.js
+++ src/index.js
@@ -10,16 +10,15 @@ class App extends React.Component{
             errorMessage: ''
         };
         
+    }
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({lat: position.coords.latitude});
-            },
-            (err) => {
-                this.setState({errorMessage: err.message});
-            }
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
         );
-    };    
-    
+    }
+
     render() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
