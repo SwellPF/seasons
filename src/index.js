144efb77e05ae2fd9f45c811d2755ab173f36b5e
Paul Fell <swellpf@gmail.com>
2021-09-12 21:35:56 -0400

    move logic for multiple returns in render to a helper function

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class App extends React.Component {
         );
     }
 
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
@@ -23,6 +23,15 @@ class App extends React.Component {
         }
         
         return <Spinner message="Attempting to Find Your Location..."/>;
+    };
+
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
